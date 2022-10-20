e37f30929ca47bbc3a90fb28a199bc60be3dd6f9
Alex Aaron <alexfrancisaaron@gmail.com>
2022-10-20 17:01:01 +0000

    added sample data to map-filter-forEach js file

--- map-filter-forEach/map-filter-forEach.js
+++ map-filter-forEach/map-filter-forEach.js
@@ -2,3 +2,53 @@
 //////////////////////////// map, filter, forEach //////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+const students = [
+    {
+        name: 'Bethany Jones',
+        age: 32,
+        gender: 'female',
+        currentCourse: 'Graduate',
+        completedCourses: ['Prep', 'Bootcamp', 'Precourse', 'Junior', 'Senior'],
+        tuitionDue: "$0"
+    },
+    {
+        name: 'Phil Connors',
+        age: 40,
+        gender: 'male',
+        currentCourse: 'Prep',
+        completedCourses: [],
+        tuitionDue: "$0"
+    },
+    {
+        name: 'Clarice Starling',
+        age: 24,
+        gender: 'female',
+        currentCourse: 'Bootcamp',
+        completedCourses: ['Prep'],
+        tuitionDue: "$800.00"
+    },
+    {
+        name: 'Lowell Bergman',
+        age: 55,
+        gender: 'male',
+        currentCourse: 'Bootcamp',
+        completedCourses: ['Prep'],
+        tuitionDue: "$800.00"
+    },
+    {
+        name: 'Jerry Maguire',
+        age: 35,
+        gender: 'male',
+        currentCourse: 'Graduate',
+        completedCourses: ['Prep', 'Bootcamp', 'Precourse', 'Junior', 'Senior'],
+        tuitionDue: "$0"
+    },
+    {
+        name: 'Craig Pelton',
+        age: 42,
+        gender: 'non-binary',
+        currentCourse: 'Prep',
+        completedCourses: ['Prep', 'Bootcamp', 'Precourse', 'Junior', 'Senior'],
+        tuitionDue: "$0"
+    }
+];
